053ae2c77933544b989362bf20dd9630af4dbca4
Jakub Dranczewski <domdlapsa@gmail.com>
2019-07-23 21:51:42 +0200

    Minificatio of html, js and css

---
+++ minify.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+html-minifier --collapse-whitespace --remove-tag-whitespace --minify-css true --minify-js true --file-ext html --input-dir . --output-dir .
+
+csso css/main.css --output css/main.css
+
+uglifyjs js/main.js > js/main.js
